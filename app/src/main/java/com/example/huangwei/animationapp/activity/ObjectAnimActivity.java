be3f2aee04a94dc6fc0032c766c392828b2cb094
huangwei <huangwei@tigerbrokers.com>
2017-08-15 17:12:43 +0800

    MOD 整理了ObjectAnimation

--- app/src/main/java/com/example/huangwei/animationapp/activity/ObjectAnimActivity.java
+++ app/src/main/java/com/example/huangwei/animationapp/activity/ObjectAnimActivity.java
@@ -1,12 +1,15 @@
 package com.example.huangwei.animationapp.activity;
 
 import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
 import android.graphics.Path;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import com.example.huangwei.animationapp.R;
 
 /**
@@ -20,6 +23,7 @@ public class ObjectAnimActivity extends AppCompatActivity implements View.OnClic
   Button btnTranslate;  //位移
   Button btnScale;      //缩放
   Button btnSet;        //集合
+  TextView textTips;    //tips
 
   @Override
   protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -31,12 +35,15 @@ public class ObjectAnimActivity extends AppCompatActivity implements View.OnClic
     btnTranslate = (Button) findViewById(R.id.btn_view_animation_translate);
     btnScale = (Button) findViewById(R.id.btn_view_animation_scale);
     btnSet = (Button) findViewById(R.id.btn_view_animation_set_first);
+    textTips = (TextView) findViewById(R.id.text_view_animation_tips_content);
 
     findViewById(R.id.btn_view_animation_set_sec).setVisibility(View.GONE);
     btnAlpha.setOnClickListener(this);
     btnRotate.setOnClickListener(this);
     btnTranslate.setOnClickListener(this);
     btnScale.setOnClickListener(this);
+    btnSet.setOnClickListener(this);
+    updateTips(getString(R.string.text_object_animation_tips_def));
   }
 
   /**
@@ -44,7 +51,7 @@ public class ObjectAnimActivity extends AppCompatActivity implements View.OnClic
    */
   protected void clickAlpha() {
     if (this.btnAlpha != null) {
-      ObjectAnimator animator = ObjectAnimator.ofFloat(this.btnAlpha, "alpha", 0, 0.2f, 0.3f, 0.7f, 1f);
+      ObjectAnimator animator = ObjectAnimator.ofFloat(this.btnAlpha, "alpha", 1f, 0.7f, 0.5f, 0.2f, 0f);
       animator.setDuration(4000);
       animator.start();
     }
@@ -59,8 +66,9 @@ public class ObjectAnimActivity extends AppCompatActivity implements View.OnClic
     if (btnRotate != null) {
       Path path = new Path();
       path.lineTo(0, 360);
+      path.lineTo(360, 360);
       ObjectAnimator animator = ObjectAnimator.ofFloat(btnRotate, "rotationX", "rotationY", path);
-      animator.setDuration(1000);
+      animator.setDuration(2000);
       animator.start();
     }
   }
@@ -69,6 +77,16 @@ public class ObjectAnimActivity extends AppCompatActivity implements View.OnClic
    * 平移
    */
   protected void clickTranslate() {
+    if (btnTranslate != null) {
+      Path path = new Path();
+      path.lineTo(0, 100);
+      path.lineTo(100, 100);
+      path.lineTo(100, 0);
+      path.lineTo(0, 0);
+      ObjectAnimator animator = ObjectAnimator.ofFloat(btnTranslate, "translationX", "translationY", path);
+      animator.setDuration(4000);
+      animator.start();
+    }
   }
 
   /**
@@ -77,13 +95,33 @@ public class ObjectAnimActivity extends AppCompatActivity implements View.OnClic
   protected void clickScale() {
     if (btnScale == null) return;
     ViewWrapper wrapper = new ViewWrapper(btnScale);
-    ObjectAnimator.ofFloat(wrapper,"width",500).setDuration(1000).start();
+    ObjectAnimator.ofFloat(wrapper, "width", 500).setDuration(1000).start();
   }
 
   /**
    * 组合
    */
   protected void clickSet() {
+    if (btnSet == null) return;
+    btnSet.setText(getString(R.string.text_view_animation_set_def));
+    PropertyValuesHolder pvh1 = PropertyValuesHolder.ofFloat("translationX", 300f);
+    PropertyValuesHolder pvh2 = PropertyValuesHolder.ofFloat("scaleX", 1f, 0, 2f);
+    PropertyValuesHolder pvh3 = PropertyValuesHolder.ofFloat("scaleY", 1f, 0, 2f);
+    ObjectAnimator.ofPropertyValuesHolder(btnSet, pvh1, pvh2, pvh3).setDuration(3000).start();
+  }
+
+  private void updateTips(String tips) {
+    if (textTips == null || TextUtils.isEmpty(tips)) return;
+    textTips.setText(tips);
+    textTips.setVisibility(View.VISIBLE);
+    findViewById(R.id.text_view_animation_tips_title).setVisibility(View.VISIBLE);
+  }
+
+  private void closeTips() {
+    if (textTips != null) {
+      textTips.setVisibility(View.GONE);
+      findViewById(R.id.text_view_animation_tips_title).setVisibility(View.GONE);
+    }
   }
 
   @Override
@@ -109,14 +147,15 @@ public class ObjectAnimActivity extends AppCompatActivity implements View.OnClic
     }
   }
 
-  public static class ViewWrapper{
+  public static class ViewWrapper {
     View target;
-    public ViewWrapper(@Nullable View view){
+
+    public ViewWrapper(@Nullable View view) {
       target = view;
     }
 
-    public void setWidth(float width){
-      target.getLayoutParams().width =(int) width;
+    public void setWidth(float width) {
+      target.getLayoutParams().width = (int) width;
       target.requestLayout();
     }
 
@@ -125,8 +164,8 @@ public class ObjectAnimActivity extends AppCompatActivity implements View.OnClic
      * layoutParams里面是wrap_content，而里面的文字还没有测量，所以一开始
      * 就是0，当measure之后才得到有文字的宽度
      */
-    public float getWidth(){
-      target.measure(0,0);
+    public float getWidth() {
+      target.measure(0, 0);
       return target.getMeasuredWidth();
       //return target.getLayoutParams().width;
     }
