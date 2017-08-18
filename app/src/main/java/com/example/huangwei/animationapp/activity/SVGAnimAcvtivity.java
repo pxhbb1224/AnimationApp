898a620bafeb08a023452d578672ae12f2cf8548
huangwei <huangwei@tigerbrokers.com>
2017-08-18 19:39:17 +0800

    MOD SVG动画1

---
+++ app/src/main/java/com/example/huangwei/animationapp/activity/SVGAnimAcvtivity.java
@@ -0,0 +1,17 @@
+package com.example.huangwei.animationapp.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * @author huangwei E-mail: huangwei@tigerbrokers.com
+ * @version 创建时间： 2017/08/18/11:13
+ */
+
+public class SVGAnimAcvtivity extends AppCompatActivity {
+  @Override
+  protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+  }
+}
